8c6eaf497fad820a72822e4ad3998f6b7b2db067
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-04-04 09:16:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
   return (
-    <div>
-      <Header />
-      <Home/>
-    </div>
+    <>
+      {loading ? (
+        <div class="flex space-x-2 justify-center items-center bg-white h-screen dark:invert">
+          <span class="sr-only">Loading...</span>
+          <div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+          <div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+          <div class="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Home />
+        </div>
+      )}
+    </>
   );
 }
 
